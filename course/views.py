7c452b9506463d014ab4e486787f4e836e81c002
accm <com.michaell@gmail.com>
2019-09-27 13:43:19 +0300

    some fix and customizations

--- course/views.py
+++ course/views.py
@@ -20,11 +20,11 @@ def index(request):
         get_init()
         from .forms import CurrencyForm
         save_currency_data.delay()
+        get_another = Course.objects.values('currency_base', 'currency_target__name', 'currency_value',
+                                            'created_date').distinct('currency_target')
 
         if request.method == 'GET':
             form = CurrencyForm()
-            get_another = Course.objects.values('currency_base', 'currency_target__name', 'currency_value',
-                                                'created_date').distinct('currency_target')
             context = {'another_currency': get_another}
 
             return render(request, 'index.html', {'context': context, 'form': form})
@@ -34,16 +34,12 @@ def index(request):
             id_another_currency = form.data.get('currency_name_first')
             another_currency = Currency.objects.filter(id=id_another_currency).first()
             get_data = get_course_json(cur_to=another_currency.code)
-            get_another = Course.objects.values_list('currency_target').distinct()
+            another_currency_list = [i for i in get_another]
             if get_data.get('success'):
                 set_schedule.delay(another_currency.code)
-                another_currency = [Course.objects.filter(currency_target=i[0]).order_by('-created_date').first() for i
-                                    in get_another]
-                context = {'another_currency': another_currency}
+                context = {'another_currency': another_currency_list}
             elif get_data.get('error'):
-                another_currency = [Course.objects.filter(currency_target=i[0]).order_by('-created_date').first() for i
-                                    in get_another]
-                context = {'another_currency': another_currency,
+                context = {'another_currency': another_currency_list,
                            'error': get_data.get('error')}
             return render(request, 'index.html', {'context': context, 'form': form})
 
